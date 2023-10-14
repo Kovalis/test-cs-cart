9f8033dbcfee8e5938f0b477d3e08d42f7f76d0a
Igor Kovalis <igor_kovalis@rambler.ru>
2023-10-14 21:45:48 +0300

    add validation & mask

--- src/blocks/form-pay/form-pay.js
+++ src/blocks/form-pay/form-pay.js
@@ -1,36 +1,68 @@
 
 import ready from "../../js/utils/documentReady.js";
-import IMask from "imask";
+// import IMask from "imask";
 import CreditCardInputMask from "credit-card-input-mask";
+import tippy from 'tippy.js';
 
 ready(function () {
 
-    const numberCard = document.querySelector('.js-card-number');
-    const numberMask = {
-        mask: Number,
-        thousandsSeparator: ' ',
-    };
-
-    // noUiSlider.create(item, {
-    //     start: [min, max],
-    //     connect: true,
-    //     range: {
-    //         'min': min,
-    //         'max': max
-    //     },
-    //     format: wNumb({
-    //         decimals: 0,
-    //         thousand: " "
-    //     })
-    // });
-    //const maskCard = IMask(numberCard, numberMask);
+    const formPay = document.querySelector('.form-pay');
+    const jsRequired = formPay.querySelectorAll(".js-required");
+    const formPaySubmit = formPay.querySelector('.js-form-pay-submit');
+    const numberCard = formPay.querySelector('.js-card-number');
+    const cvvCard = formPay.querySelector('.js-card-cvv');
+    let flagSentForm = true;
 
     const formattedCreditCardInput = new CreditCardInputMask({
         element: numberCard,
         pattern: "{{9999}} {{9999}} {{9999}} {{9999}}",
     });
 
-    // numberCard.addEventListener('change', () =>{
-    //     maskCard.updateValue();
-    // });
+    const formattedCvvCard = new CreditCardInputMask({
+        element: cvvCard,
+        pattern: "{{999}}",
+    });
+
+    tippy('[data-tippy-content]');
+
+    function checkedInputs(){
+        flagSentForm = true;
+        const itemsInput = formPay.querySelectorAll("input[required]");
+        itemsInput.forEach((item)=>{
+            if(item.type == "checkbox"){
+                if(item.checked == false){
+                    item.closest(".js-required").classList.add("required-error");
+                    flagSentForm = false;
+                } else{
+                    item.closest(".js-required").classList.remove("required-error");
+                }
+            } else{
+                if(item.value == ""){
+                    item.closest(".js-required").classList.add("required-error");
+                    flagSentForm = false;
+                } else{
+                    item.closest(".js-required").classList.remove("required-error");
+                }
+            }
+        });
+
+        if(flagSentForm){
+            console.log(formPaySubmit)
+            formPaySubmit.submit();
+        } else{
+            return false;
+        }
+    }
+
+    jsRequired.forEach((element)=>{
+        element.addEventListener("focus", ()=>{
+            element.classList.remove("required-error");
+        });
+    });
+
+
+    formPaySubmit.addEventListener("submit", (event)=>{
+        event.preventDefault();
+        checkedInputs();
+    });
 });
